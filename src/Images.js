00853f81f8fff0354e134d7bb13cb40a37a12ff5
DA M1 59 <dam159@DA-learners-MacBook-Air.local>
2022-07-22 09:34:13 +0200

    added index.css

---
+++ src/Images.js
@@ -0,0 +1,23 @@
+import Images01 from './memes/cat.jpg';
+import Images02 from './memes/dog.jpg';
+import Images03 from './memes/ghost.jpg';
+import Images04 from './memes/bus.jpg';
+import Images05 from './memes/iphone.jpg';
+import Images06 from './memes/money.jpg';
+import Images07 from './memes/mouse.jpg';
+import Images08 from './memes/gun.jpg';
+import Images09 from './memes/movies.jpg';
+import Images10 from './memes/congratulation.jpg';
+import Images11 from './memes/play.jpg';
+import Images12 from './memes/water.jpg';
+import Images13 from './memes/rose.jpg';
+import Images14 from './memes/run.jpg';
+import Images15 from './memes/accident.jpg';
+import Images16 from './memes/smile.jpg';
+import Images17 from './memes/tree.jpg';
+import Images18 from './memes/water.jpg';
+
+
+const Images = [Images01, Images02, Images03,Images04,Images05,Images06,Images07,Images08,Images09,Images10,Images11,Images12,Images13,Images14,Images15,Images16,Images17,Images18];
+
+export default Images;
